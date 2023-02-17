57bcadbc0dcd46118e9f4cfbd6093a30b1c1812c
gkojcko <116667495+gkojcko@users.noreply.github.com>
2023-02-17 14:24:55 +0200

    Create Caclulator.java

---
+++ Carpet_Calculator/Caclulator.java
@@ -0,0 +1,16 @@
+package project.testing;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet){
+        this.floor = floor;
+        this.carpet = carpet;
+
+    }
+
+    public double getTotalCost(){
+        return floor.getArea() * carpet.getCost();
+    }
+}
