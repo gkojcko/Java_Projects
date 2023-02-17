a7a6d93fb4bb542c3357e9c722ca56bcdd9b81fd
gkojcko <116667495+gkojcko@users.noreply.github.com>
2023-02-17 14:24:45 +0200

    Create Carpet.java

---
+++ Carpet_Calculator/Carpet.java
@@ -0,0 +1,17 @@
+package project.testing;
+
+
+public class Carpet {
+    private double cost;
+    Carpet(double cost){
+        this.cost = cost;
+        if(cost <=0){
+            this.cost = 0;
+        }
+
+    }
+    public double getCost() {
+        return cost;
+    }
+}
+
