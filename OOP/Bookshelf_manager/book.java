65ade671cf395885804ab8a1b900842acb1a67aa
gkojcko <116667495+gkojcko@users.noreply.github.com>
2023-03-10 00:27:02 +0200

    Create book.java

---
+++ OOP/Bookshelf_manager/book.java
@@ -0,0 +1,45 @@
+package project.testing.bank.package2;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public void blankFields(){
+        setTitle(" ");
+        setAuthor(" ");
+        setGenre(" ");
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
